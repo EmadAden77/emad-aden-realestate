f0a3842b560012188c00f483fc0a540ffbb721ea
EmadAden77 <sasfsas8@gmail.com>
2026-04-12 20:19:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -42,4 +42,51 @@ self.addEventListener('activate', (event) => {
 
 
 ```
+```javascript
+const CACHE_NAME = 'emad-realestate-v2';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/data.json'
+];
+
+// تثبيت ملفات الكاش للعمل بدون إنترنت
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// اعتراض الطلبات وإرجاعها من الكاش إذا كان النت مفصول
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// تنظيف الكاش القديم
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+```
 
